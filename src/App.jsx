ac1737c74915b27b9cb86d67c7e8a6ae59c21962
Ricardo <ricardoxsobral@gmail.com>
2024-07-08 11:14:54 -0300

    [feat] Adding footer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 
 import Navbar from './components/navbar/Navbar'
 import Hero from './components/hero/Hero'
+import Footer from './components/footer/Footer'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <div className='App'>
         <Navbar />
         <Hero />
+        <Footer />
       </div>
     </>
   )
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,10 @@
+import Container from '@mui/material/Container';
+
+import iconFone from '../../assets/icon-fone.png'
+import iconMessage from '../../assets/icon-message.png'
+import iconCorebiz from '../../assets/corebiz-branco.png'
+import iconVetex from '../../assets/vetex.png'
+
 import './Footer.css'
 
 function Footer() {
@@ -5,7 +12,54 @@ function Footer() {
   return (
     <>
       <div className='Footer'>
-        
+        <Container>
+          <div className="footer-content">
+            <div className="footer-item info">
+              <h1>Localização</h1>
+              <hr />
+              <div className="info-item addres">
+                <span>Avenida Andrômeda, 2000. Bloco 6 e 8 <br /> </span>
+                <span>Alphaville - SP</span>
+              </div>
+              <div className="info-item email">
+                <span>brasil@corebiz.ag</span>
+              </div>
+              <div className="info-item telephone">
+                <span>+55 11 3090 1039</span>
+              </div>
+            </div>
+            <div className="footer-item cta">
+              <div className="btn-cta">
+                <div className="icon">
+                  <img src={iconMessage} alt="icon-message" />
+                </div>
+                <div className="text">
+                  <span>ENTRE EM CONTATO</span>
+                </div>
+              </div>
+              <div className="btn-cta">
+                <div className="icon">
+                  <img src={iconFone} alt="icon-message" />
+                </div>
+                <div className="text">
+                  <span className='text1'>FALE COM NOSSO</span>
+                  <span>CONSULTOR ONLINE</span>
+                </div>
+              </div>
+            </div>
+            <div className="footer-item">
+              <div className="logo-footer">
+                <div className="logo">
+                  <span>Created by </span>
+                  <img src={iconCorebiz} alt="corebiz-white-logo" />
+                </div>
+                <div className="logo">
+                  <img src={iconVetex} alt="vetex-logo" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </Container>
       </div>
     </>
   )
